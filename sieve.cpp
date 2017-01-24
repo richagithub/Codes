4c3a14f2728196ea5bce7ef0cf5c59b39e724c96
richagithub <richa09me@gmail.com>
2017-01-24 17:49:54 +0530

    Create sieve.cpp
    To calculate prime factorizations of any number , we pre-calculate sieve table as sp[]:-
    For each number we store the "smallest prime number that divides i "
    To calculate factorize of a no, N , we keep doing  k=N/sp[N]  =>  then new k generated m= k/sp[k] => => m/sp[m]..... untill reduced to 1

---
+++ sieve.cpp
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<conio.h>
+#include<iostream>
+#include<vector>
+#define lli long long int
+#define MAX 100
+using namespace std;
+
+bool v[MAX];
+int len, sp[MAX];
+
+void Sieve(){
+	for (int i = 2; i < MAX; i += 2)	sp[i] = 2;//even numbers have smallest prime factor 2
+	for (lli i = 3; i < MAX; i += 2){
+		if (!v[i]){
+			sp[i] = i;
+			for (lli j = i; (j*i) < MAX; j += 2){
+				if (!v[j*i])	v[j*i] = true, sp[j*i] = i;
+			}
+		}
+	}
+}
+
+vector <int> factorize(int k) {               //this is how to factorize k. //result is stored in vector<int>
+	vector <int> ans;
+	while(k>1) {
+		ans.push_back(sp[k]);
+		k/=sp[k];
+	}
+	return ans;
+}
+
+int main(){
+	Sieve();
+	for (int i = 0; i < 50; i++)	cout << sp[i] << "\t";
+
+    return 0;
+}
